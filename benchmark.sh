d9c12913df353e29c741dfa3deb94936a8f0e9dd
ZeroBone <zerobone21@gmail.com>
2023-03-02 02:44:05 +0100

    Benchmark generation script

---
+++ benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source venv/bin/activate;
+
+for i in {1..200}; do
+  python vardec/benchmark_gen_add "$i";
+done
+
+for i in {1..200}; do
+  python vardec/benchmark_gen_spaces.py "$i";
+done
+
+deactivate;
